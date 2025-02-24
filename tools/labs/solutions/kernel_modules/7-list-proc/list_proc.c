e78ba75cff1592d7f6058c8ed1be6978c792689b
rkhapov <r.khapov@ya.ru>
2025-02-24 16:33:15 +0000

    7-list-proc solution
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- tools/labs/solutions/kernel_modules/7-list-proc/list_proc.c
+++ tools/labs/solutions/kernel_modules/7-list-proc/list_proc.c
@@ -1,7 +1,8 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-/* TODO: add missing headers */
+#include <linux/sched.h>
+#include <linux/printk.h>
 
 MODULE_DESCRIPTION("List current processes");
 MODULE_AUTHOR("Kernel Hacker");
@@ -9,18 +10,22 @@ MODULE_LICENSE("GPL");
 
 static int my_proc_init(void)
 {
-	struct task_struct *p;
+	struct task_struct *p = get_current();
 
-	/* TODO: print current process pid and its name */
+	printk(KERN_INFO "Current process pid: %d, comm = %s\n", p->pid, p->comm);
 
-	/* TODO: print the pid and name of all processes */
+	for_each_process(p) {
+		printk(KERN_INFO "There is process pid: %d, comm = %s\n", p->pid, p->comm);
+	}
 
 	return 0;
 }
 
 static void my_proc_exit(void)
 {
-	/* TODO: print current process pid and name */
+	struct task_struct *p = get_current();
+
+	printk(KERN_INFO "Current process pid: %d, comm = %s\n", p->pid, p->comm);
 }
 
 module_init(my_proc_init);
