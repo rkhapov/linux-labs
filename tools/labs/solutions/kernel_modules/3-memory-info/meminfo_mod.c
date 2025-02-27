b02ab4aedadee2d1e9ee096ef111a002f2baf3b6
rkhapov <r.khapov@ya.ru>
2025-02-27 10:49:47 +0000

    3-meminfo solution
    Signed-off-by: rkhapov <r.khapov@ya.ru>

---
+++ tools/labs/solutions/kernel_modules/3-memory-info/meminfo_mod.c
@@ -0,0 +1,51 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/list.h>
+#include <linux/pid.h>
+#include <linux/mm.h>
+#include <linux/sched/mm.h>
+#include <linux/mm_types.h>
+#include <linux/printk.h>
+
+MODULE_DESCRIPTION("List current process vmas");
+MODULE_AUTHOR("Kernel Hacker");
+MODULE_LICENSE("GPL");
+
+noinline void print_mmap(struct task_struct*);
+
+noinline void print_mmap(struct task_struct *p) {
+	printk(KERN_INFO "Process pid: %d, comm = %s\n", p->pid, p->comm);
+
+	struct mm_struct *mm = get_task_mm(p);
+	struct vm_area_struct *vma;
+	VMA_ITERATOR(vmi, mm, 0);
+
+	mmap_read_lock(mm);
+	for_each_vma(vmi, vma) {
+		printk(KERN_INFO "[%lx..%lx)", vma->vm_start, vma->vm_end);
+	}
+	mmap_read_unlock(mm);
+}
+
+static int my_proc_init(void)
+{
+	struct task_struct *p = get_current();
+
+	print_mmap(p);
+
+	for_each_process(p) {
+		print_mmap(p);
+	}
+
+	return 0;
+}
+
+static void my_proc_exit(void)
+{
+	pr_debug("BB");
+}
+
+module_init(my_proc_init);
+module_exit(my_proc_exit);
