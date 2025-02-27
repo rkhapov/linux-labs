09bb1af330cc83df34b12b2bc0453b999b7080b8
rkhapov <r.khapov@ya.ru>
2025-02-27 13:07:56 +0000

    fix 3-meminfo
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- tools/labs/solutions/kernel_modules/3-memory-info/meminfo_mod.c
+++ tools/labs/solutions/kernel_modules/3-memory-info/meminfo_mod.c
@@ -19,6 +19,10 @@ noinline void print_mmap(struct task_struct *p) {
 	printk(KERN_INFO "Process pid: %d, comm = %s\n", p->pid, p->comm);
 
 	struct mm_struct *mm = get_task_mm(p);
+	if (mm == NULL) {
+		printk(KERN_INFO "No task mm set\n");
+		return;
+	}
 	struct vm_area_struct *vma;
 	VMA_ITERATOR(vmi, mm, 0);
 
@@ -27,6 +31,8 @@ noinline void print_mmap(struct task_struct *p) {
 		printk(KERN_INFO "[%lx..%lx)", vma->vm_start, vma->vm_end);
 	}
 	mmap_read_unlock(mm);
+
+	mmput(mm);
 }
 
 static int my_proc_init(void)
@@ -34,6 +40,8 @@ static int my_proc_init(void)
 	struct task_struct *p = get_current();
 
 	print_mmap(p);
+
+	p = get_current();
 
 	for_each_process(p) {
 		print_mmap(p);
